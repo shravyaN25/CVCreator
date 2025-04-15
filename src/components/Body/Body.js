3a3313526013112683105f5e8cca16cbef756e59
Shravya N <109436935+shravyaN25@users.noreply.github.com>
2025-04-15 18:07:39 +0530

    Update Body.js

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -8,7 +8,6 @@ import Resume from "../Resume/Resume";
 import styles from "./Body.module.css";
 
 function Body() {
-  const colors = ["#239ce2", "#48bb78", "#0bc5ea", "#a0aec0", "#ed8936"];
   const sections = {
     basicInfo: "Basic Info",
     workExp: "Work Experience",
